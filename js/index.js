2b2c8fbbbbe2a29a623d05912f2337c1f71e83b5
nomayen <nomayen.ohin@gmail.com>
2023-10-12 21:34:34 +0600

    dor off and data slice

--- js/index.js
+++ js/index.js
@@ -1,20 +1,37 @@
-const handleCategory =  async () =>{
+const handleCategory = async () => {
     const response = await fetch(' https://openapi.programming-hero.com/api/news/categories')
-    
+
     const data = await response.json();
 
     const tabContainer = document.getElementById('tab-container')
+    const categoryData = data.data.news_category;
 
-    data.data.news_category.slice(0,3).forEach((category) => {
-        console.log(category);
+    //    categoryData.slice(0,3).forEach((category) => {
+    //         console.log(category);
+    //         const div = document.createElement('div');
+    //         div.innerHTML = `
+    //         <a class="tab">${category.category_name}</a> 
+    //         `
+    //         tabContainer.appendChild(div);
+
+    //     });
+    for (const category of categoryData) {
+        // console.log(category);
+        const sliceData = categoryData.slice(0,3);
+        console.log(sliceData);
+
         const div = document.createElement('div');
-        div.innerHTML = `
-        <a class="tab">${category.category_name}</a> 
-        `
-        tabContainer.appendChild(div);
-        
-    });
-    console.log(data.data.news_category);
+            div.innerHTML = `
+            <a class="tab">${category.category_name}</a> 
+            `
+            tabContainer.appendChild(div);
+
+    }
+    // console.log(categoryData);
 }
 
 handleCategory()
+
+
+
+// 47 min 
