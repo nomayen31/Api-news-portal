f960d513c4a0052d089dddbd354bf7e2d51da2f2
nomayen <nomayen.ohin@gmail.com>
2023-10-12 21:12:44 +0600

    api called and category show on nav

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 const handleCategory =  async () =>{
     const response = await fetch(' https://openapi.programming-hero.com/api/news/categories')
+    
+    const data = await response.json();
 
-    const data = await response.json()
-    console.log(data);
+    const tabContainer = document.getElementById('tab-container')
+
+    data.data.news_category.slice(0,3).forEach((category) => {
+        console.log(category);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <a class="tab">${category.category_name}</a> 
+        `
+        tabContainer.appendChild(div);
+        
+    });
+    console.log(data.data.news_category);
 }
 
 handleCategory()
