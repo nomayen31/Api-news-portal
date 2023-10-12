ea20771e09d4bea709310c58fa1ea479905da0b0
nomayen <nomayen.ohin@gmail.com>
2023-10-12 20:34:48 +0600

    api connect

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-const 
+const handleCategory =  async () =>{
+    const response = await fetch(' https://openapi.programming-hero.com/api/news/categories')
+
+    const data = await response.json()
+    console.log(data);
+}
+
+handleCategory()
